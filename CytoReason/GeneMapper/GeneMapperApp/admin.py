aa0a1a38be985491dfd55221d5c39887193d2680
Ziv Kitlaro <zivale@gmail.com>
2018-01-01 13:45:01 +0200

    re-upload without db, trying to LFS it

--- CytoReason/GeneMapper/GeneMapperApp/admin.py
+++ CytoReason/GeneMapper/GeneMapperApp/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
+from .models import Alias
 from .models import GeneInfo
 
-admin.site.register(GeneInfo)
+
+class GeneInfoAdmin(admin.ModelAdmin):
+	list_display = ['id', 'gene_name', 'symbol']
+
+admin.site.register(GeneInfo, GeneInfoAdmin)
+
+class AliasAdmin(admin.ModelAdmin):
+	list_display = ['id', 'gene', 'alias_symbol']
+
+admin.site.register(Alias, AliasAdmin)
+
+
+
+
+# admin.site.register(GeneAliases, GeneAliasesAdmin)
--- CytoReason/GeneMapper/GeneMapperApp/models.py
+++ CytoReason/GeneMapper/GeneMapperApp/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+
 class GeneInfo(models.Model):
-    _id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     gene_name = models.TextField()
     symbol = models.TextField()
-
     class Meta:
-		db_table = 'gene_info'
+    	db_table = 'gene_info'
 
+class Entres(models.Model):
+	id = models.IntegerField(primary_key=True)
+	gene = models.ForeignKey(GeneInfo, related_name='entrez', primary_key=True)
+	entrez_id = models.TextField()
+	class Meta:
+		db_table = 'genes'
 
+class Alias(models.Model):
+	id = models.IntegerField(primary_key=True)
+	alias_symbol = models.TextField()
+	gene = models.ForeignKey(GeneInfo, related_name='aliases')
+
+	class Meta:
+		db_table = 'alias'
